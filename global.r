81bbcb6f0104033fdd04858fb0b33562a4cc4351
BDalheimer <bernhard.dalheimer@hotmail.com>
2015-11-12 18:03:25 +0100

    added some table reshapes

--- global.r
+++ global.r
@@ -23,21 +23,24 @@ suaLong <- dataSUA[dataSUA$Element %in% elementsSua,]
 ## Couldn't find the duplicate elements. Also, the data is incomplete. I started working with a subset of only one country 
 sua <- dcast.data.table(suaLong, geographicArea + timePointYears + Item ~ Element, value.var="Value")
 
-suaElementNames <- c("Production", "Import", "Export", "Stocks", "Feed Use", "Food Use", "Seed", "Food Losses & Waste", 
-                     "Tourist Consumption", "Industrial Use")
+suaElementNames <- c("Production", "Import", "Export", "Stocks", "Food Use", "Feed Use","Seed", "Food Losses & Waste", 
+                     "Tourist Consumption", "Industrial Use", "Residual Other Uses")
 
+suaElementCodes = c(5510, 5610, 5910, 5071, 5141, 5520, 5525, 5015, 5164, 5165, 5166)
 
-productionElements = c(5510, 55100, 55101, 5515)
+suaElementTable = data.table(measuredElement = suaElementCodes, Element = suaElementNames)
 
-feedElements = c(5520)
-
-seedElements = c(5520)
-
-otherElements = c(5153)
+# productionElements = c(5510, 55100, 55101, 5515)
+# 
+# feedElements = c(5520)
+# 
+# seedElements = c(5520)
+# 
+# otherElements = c(5153)
 
 
 ## create empty datasets for each sua element, which will be filled by user or R module
-emptyData <- data.frame(Commodity = unique(data$Item), ExpectedValue = rep(0, length(unique(data$Item))),
+emptyData = data.frame(Commodity = unique(data$Item), ExpectedValue = rep(0, length(unique(data$Item))),
                         UpperBound = rep(0, length(unique(data$Item))), 
                         LowerBound = rep(0, length(unique(data$Item))),
                         stringsAsFactors = FALSE, row.names = unique(data$measuredItemCPC))
@@ -48,6 +51,12 @@ for(i in suaElementNames){
   (assign(paste0(i), emptyData))
   
 }
+
+#for(i in suaElementTable[, suaElementCodes]) {
+#  if(nrow(data[measuredElement == i, ]) != 0)
+#  assign(paste(suaElementTable[measuredElement == i, Element]), 
+#         dcast.data.table(data[measuredElement == i, ], measuredItemCPC + Item ~ timePointYears, value.var = "Value"))
+#}
 setkey(data)
 
 
