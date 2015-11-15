541431cf2a79d0f42fe2e85b622e3c0b00752dd7
BDalheimer <bernhard.dalheimer@hotmail.com>
2015-11-15 18:38:32 +0100

    created second set of select inputs

---
+++ R/selectizeBrowseData.R
@@ -0,0 +1,31 @@
+fluidRow(
+  column(3, 
+        selectInput(
+          "M49GeographicArea", 
+          "Enter Area:", 
+          c("All", apply(unique(data[, .(geographicAreaM49, geographicArea)]), 1, paste, collapse= " | ")), 
+          multiple = TRUE, selectize = F)
+), 
+
+column(3, 
+       selectizeInput(
+         "codeElment", 
+         "Enter Elements:", 
+         choices = apply(unique(data[, .(measuredElement, Element)]), 1, paste, collapse= " | "), 
+         multiple = TRUE)
+),
+column(4, 
+       selectizeInput(
+         "CPCItem", 
+         "Enter Items:", 
+         choices = apply(unique(data[, .(measuredItemCPC, Item)]), 1, paste, collapse= " | "), 
+         multiple = TRUE)
+),
+column(2, 
+       selectizeInput(
+         "codeYear", 
+         "Enter Years:", 
+         choices = unique(data[, timePointYears]), 
+         multiple = TRUE)
+)
+)
