63a1bfb55992849769b8529ed534ceb66eccb578
BDalheimer <bernhard.dalheimer@hotmail.com>
2015-12-13 20:45:21 +0100

    observers to save input. rhandsontable are entirely reactive now.  This closes #3

---
+++ R/observeSaveRestore.R
@@ -0,0 +1,24 @@
+# This function saves data if reqested and restores to default data if ordered so
+#
+observeSaveRestore = function(input, output, session){
+
+observeEvent(input$saveSave , {
+  if(!is.null(input$tableProduction)){
+    
+    data <<- saveToData(input, output, session, individualSUATables)()
+    
+    saveRDS(saveToData(input, output, session, individualSUATables)(), file = "Data/testData.rda")}
+}
+)
+
+observeEvent( input$restoreDefault, {
+  
+  
+  data <<- readRDS("Data/backUpTestData.rda")
+  
+  saveRDS(data, file = "Data/testData.rda")
+  createAlert(session, "restartShiny", "makeEffectiveAllert", title = "Restart",
+              content = "You need to restart the FBS compiler to make changes effective.", append = FALSE)}
+)
+
+}
